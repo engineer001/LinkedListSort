909c440f7a56fe37f44d61545cd8a5ad7f6cf3fb
engineer001 <35121579+engineer001@users.noreply.github.com>
2018-04-12 21:20:07 +0800

    Create linkedlistsort.c
    .c file that carry out the function

---
+++ linkedlistsort.c
@@ -0,0 +1,16 @@
+#include <linkedlistsort.h>
+
+typedef struct Node
+{
+  int data;
+  struct Node* next;
+};
+
+Node* initLinkedList()
+{
+  Node* head=(Node*)malloc(sizeof(Node));
+
+int main()
+{
+  
+}
