c5fa0f895e772919beddc71297575ed399aff538
engineer001 <35121579+engineer001@users.noreply.github.com>
2018-04-12 22:37:53 +0800

    Update linkedlistsort.c

--- linkedlistsort.c
+++ linkedlistsort.c
@@ -4,11 +4,21 @@ typedef struct Node
 {
   int data;
   struct Node* next;
-};
+}Node;
 
 Node* initLinkedList()
 {
   Node* head=(Node*)malloc(sizeof(Node));
+  head->next=NULL;
+  
+  int temp;
+  while(scanf("%d",&temp)!=EOF)
+  {
+    Node* node=(Node*)malloc(sizeof(Node));
+    node->data=temp;
+    node->next=head->next;
+    head->next=node;
+  }
 
 int main()
 {
