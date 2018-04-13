143cdd296d3d91b7403576855d3dd5a2b69d87fc
engineer001 <35121579+engineer001@users.noreply.github.com>
2018-04-13 22:05:25 +0800

    Update linkedlistsort.c

--- linkedlistsort.c
+++ linkedlistsort.c
@@ -9,6 +9,7 @@ typedef struct Node
 Node* initLinkedList()
 {
   Node* head=(Node*)malloc(sizeof(Node));
+  head->data=0;
   head->next=NULL;
   
   int temp;
@@ -19,7 +20,28 @@ Node* initLinkedList()
     node->next=head->next;
     head->next=node;
   }
+  return head;
+}
 
+Node* sortLinkedList(Node* Linkedlist)
+{
+  for(Node* p=Linkedlist->next;p->next!=NULL;p=p->next)
+    for(Node* q=p->next;q->next!=NULL;q=q->next)
+    {
+      if((p->data)>(q->data))
+      {
+        int temp=p->data;
+        p->data=q->data;
+        q->data=temp;
+      }
+    }
+}
+
+void printLinkedList(Node* Linkedlist)
+{
+   
+}
+  
 int main()
 {
   
